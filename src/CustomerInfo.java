c666080d118d821c2501e9dd8873260ab2cf5fee
hibaS02 <hiba.021@outlook.com>
2021-11-28 00:33:41 +0200

    Shop Updated

--- src/CustomerInfo.java
+++ src/CustomerInfo.java
@@ -1,7 +1,9 @@
+import java.util.*;
 
 public class CustomerInfo {
-private String firstName, lastName, gender, status, location, contact;
-	
+	private String firstName, lastName, gender, status, location;
+	private int contact;
+	Scanner input = new Scanner(System.in);
 	public CustomerInfo() {
 	
 		firstName = "";
@@ -9,10 +11,10 @@ private String firstName, lastName, gender, status, location, contact;
 		gender = "";
 		status = "";
 		location = "";
-		contact = "";
+		contact = 0;
 	}
 	
-	public void setCustomer(String fN, String lN, String g, String s, String l, String c){
+	public void setCustomer(String fN, String lN, String g, String s, String l, int c){
 	
 		firstName = fN;
 		lastName = lN;
@@ -23,10 +25,14 @@ private String firstName, lastName, gender, status, location, contact;
 	}
 	
 	public String getFirstName() {
+		System.out.print("First Name: ");
+		String firstName = input.next();
 		return firstName;
 	}
 	
 	public String getLastName() {
+		System.out.print("Last Name: ");
+		String lastName = input.next();
 		return lastName;
 	}
 	
@@ -34,19 +40,45 @@ private String firstName, lastName, gender, status, location, contact;
 		return getFirstName() + " " + getLastName();
 	}
 	
+	
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
 	public String getGender() {
+		System.out.print("Gender: ");
+		String gender = input.next();
 		return gender;
 	}
 	
 	public String getStatus() {
+		System.out.print("Marital status: ");
+		String status = input.next();
 		return status;
 	}
 	
 	public String getLocation() {
+		System.out.print("Address:");
+		String location = input.next();
 		return location;
 	}
 	
-	public String getContact() {
-		return contact;
+	public void getContact() {
+		System.out.print("Phone number: ");
+		int contact =input.nextInt();
+		
+		
+	}
+	public void display() {
+		System.out.print("Welcome ");
+		//String gender = getGender();
+		String m= "male";//hek azdik?btzbt ?a
+		if (getGender()=="m") {// haide el gender hon bade yehaa tekhod el input value li hatta el user bl getGender method 
+			System.out.print("Mr. "+firstName+" "+lastName);
+			System.out.println(getGender());
+		}else {
+			System.out.println("sherke");// w ha krml shuf shu aam ysir 5ALINE EKBOS ELE SE3A 3AM JARIB ez AHHAHAH ok rja3e farjine sorry aam tawil 3leik aatine 1 min eza b3d bet2ule sorry 7a shutik ma fik w ma fiyyye ma oul sorry l2n aam bkhdlk mn wa2tak aal fade 
+			// okay so
+		}//okay fike tzakrine shu ken badik ta3mle?  bade haide el gender taamol referc=nce aa ha 
 	}
 }
--- src/Shop.java
+++ src/Shop.java
@@ -14,6 +14,22 @@ public class Shop {
 		this.customers=customers;
 		current=0;
 	}
+	public void WelcomeNote() {//FARJINE WEN 
+		System.out.println("Welcome to Rent A Car! \nWould you like to rent now?");
+		String ans= input.next();
+		String y="yes";
+		String n="no";
+		if(ans.equals(n)) {
+			System.out.println("Thank you for visting our shop! Have a look around.");
+		}else if(ans.equals(y)) {
+			System.out.println("Please fill in the form below to start your rental process:");
+			CustomerInfo CI = new CustomerInfo();
+			CI.getFirstName(); CI.getLastName(); CI.getGender(); CI.getStatus(); CI.getLocation(); CI.getContact();
+			//CI.display();
+		}// KIFFF ESH3AAAAA
+		// fike tfute ente di2a la 3ende la rjike l fare2? deal senye tyt
+		
+	}
 	public boolean isEmpty() {
 		if (current==0) 
 			return true;
